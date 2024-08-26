598a152fef91cc5dd6f1b1afece4808a3a7cfe6a
satyam <ankitmodi117@gmail.com>
2024-08-26 13:57:09 +0530

    add code server.js file

--- auth-app-backend/server.js
+++ auth-app-backend/server.js
@@ -23,3 +23,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+export default (req, res) => {
+  res.status(200).json({ message: 'Hello from Vercel!' });
+};
